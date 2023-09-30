200f7df12184ba3048940bdb2af31190f6143d9f
robynainsley21 <robynainsley2i@gmail.com>
2023-09-30 12:21:59 +0200

    Added basic navbar

--- app.js
+++ app.js
@@ -10,11 +10,24 @@ const app = express();
 //returns an instance of the server
 app.listen(8080);
 
+/**
+ * first argument is the path, second argument is a function that is fired once at that location
+ */
 app.get('/', (req, res) => {
     /**
      * - infers the type of content that one is trying to send to the browser and automatically sets
      * the content type header [res.setHeader('Content-Type', 'text/html') in server.js]
      * - it also sets the status code
      */
-    res.send('<p>home page</p>');
+    // res.send('<p>home page</p>');
+
+    /**
+     * The first argument is just a relative path, not the proper path of the file, therefore
+     * the second argument states the current directory of the file
+     */
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname});
 });
